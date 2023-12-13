99e8597a09d58e26b3411bc2e8a092b5ec21dbdf
sukhdevKolli <74062374+sukhdevKolli@users.noreply.github.com>
2023-12-12 22:44:47 -0500

    Update dataset_tool.py

--- dataset_tool.py
+++ dataset_tool.py
@@ -28,46 +28,46 @@ import numpy as np
 import PIL.Image
 from tqdm import tqdm
 
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 
 def error(msg):
+    """Display an error message and exit the program."""
     print('Error: ' + msg)
     sys.exit(1)
 
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 
 def parse_tuple(s: str) -> Tuple[int, int]:
-    '''Parse a 'M,N' or 'MxN' integer tuple.
-
-    Example:
-        '4x2' returns (4,2)
-        '0,1' returns (0,1)
-    '''
+    """Parse a 'M,N' or 'MxN' integer tuple."""
     if m := re.match(r'^(\d+)[x,](\d+)$', s):
         return (int(m.group(1)), int(m.group(2)))
     raise ValueError(f'cannot parse tuple {s}')
 
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 
 def maybe_min(a: int, b: Optional[int]) -> int:
+    """Return the minimum of two values, handling the case where one is None."""
     if b is not None:
         return min(a, b)
     return a
 
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 
 def file_ext(name: Union[str, Path]) -> str:
+    """Get the file extension from a file path."""
     return str(name).split('.')[-1]
 
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 
 def is_image_ext(fname: Union[str, Path]) -> bool:
+    """Check if a file has an image extension."""
     ext = file_ext(fname).lower()
     return f'.{ext}' in PIL.Image.EXTENSION # type: ignore
 
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 
 def open_image_folder(source_dir, *, max_images: Optional[int]):
+    """Open an image folder and load images along with labels."""
     input_images = [str(f) for f in sorted(Path(source_dir).rglob('*')) if is_image_ext(f) and os.path.isfile(f)]
 
     # Load labels.
@@ -77,7 +77,7 @@ def open_image_folder(source_dir, *, max_images: Optional[int]):
         with open(meta_fname, 'r') as file:
             labels = json.load(file)['labels']
             if labels is not None:
-                labels = { x[0]: x[1] for x in labels }
+                labels = {x[0]: x[1] for x in labels}
             else:
                 labels = {}
 
@@ -89,136 +89,23 @@ def open_image_folder(source_dir, *, max_images: Optional[int]):
             arch_fname = arch_fname.replace('\\', '/')
             img = np.array(PIL.Image.open(fname))
             yield dict(img=img, label=labels.get(arch_fname))
-            if idx >= max_idx-1:
-                break
-    return max_idx, iterate_images()
-
-#----------------------------------------------------------------------------
-
-def open_image_zip(source, *, max_images: Optional[int]):
-    with zipfile.ZipFile(source, mode='r') as z:
-        input_images = [str(f) for f in sorted(z.namelist()) if is_image_ext(f)]
-
-        # Load labels.
-        labels = {}
-        if 'dataset.json' in z.namelist():
-            with z.open('dataset.json', 'r') as file:
-                labels = json.load(file)['labels']
-                if labels is not None:
-                    labels = { x[0]: x[1] for x in labels }
-                else:
-                    labels = {}
-
-    max_idx = maybe_min(len(input_images), max_images)
-
-    def iterate_images():
-        with zipfile.ZipFile(source, mode='r') as z:
-            for idx, fname in enumerate(input_images):
-                with z.open(fname, 'r') as file:
-                    img = PIL.Image.open(file) # type: ignore
-                    img = np.array(img)
-                yield dict(img=img, label=labels.get(fname))
-                if idx >= max_idx-1:
-                    break
-    return max_idx, iterate_images()
-
-#----------------------------------------------------------------------------
-
-def open_lmdb(lmdb_dir: str, *, max_images: Optional[int]):
-    import cv2  # pip install opencv-python # pylint: disable=import-error
-    import lmdb  # pip install lmdb # pylint: disable=import-error
-
-    with lmdb.open(lmdb_dir, readonly=True, lock=False).begin(write=False) as txn:
-        max_idx = maybe_min(txn.stat()['entries'], max_images)
-
-    def iterate_images():
-        with lmdb.open(lmdb_dir, readonly=True, lock=False).begin(write=False) as txn:
-            for idx, (_key, value) in enumerate(txn.cursor()):
-                try:
-                    try:
-                        img = cv2.imdecode(np.frombuffer(value, dtype=np.uint8), 1)
-                        if img is None:
-                            raise IOError('cv2.imdecode failed')
-                        img = img[:, :, ::-1] # BGR => RGB
-                    except IOError:
-                        img = np.array(PIL.Image.open(io.BytesIO(value)))
-                    yield dict(img=img, label=None)
-                    if idx >= max_idx-1:
-                        break
-                except:
-                    print(sys.exc_info()[1])
-
-    return max_idx, iterate_images()
-
-#----------------------------------------------------------------------------
-
-def open_cifar10(tarball: str, *, max_images: Optional[int]):
-    images = []
-    labels = []
-
-    with tarfile.open(tarball, 'r:gz') as tar:
-        for batch in range(1, 6):
-            member = tar.getmember(f'cifar-10-batches-py/data_batch_{batch}')
-            with tar.extractfile(member) as file:
-                data = pickle.load(file, encoding='latin1')
-            images.append(data['data'].reshape(-1, 3, 32, 32))
-            labels.append(data['labels'])
-
-    images = np.concatenate(images)
-    labels = np.concatenate(labels)
-    images = images.transpose([0, 2, 3, 1]) # NCHW -> NHWC
-    assert images.shape == (50000, 32, 32, 3) and images.dtype == np.uint8
-    assert labels.shape == (50000,) and labels.dtype in [np.int32, np.int64]
-    assert np.min(images) == 0 and np.max(images) == 255
-    assert np.min(labels) == 0 and np.max(labels) == 9
-
-    max_idx = maybe_min(len(images), max_images)
-
-    def iterate_images():
-        for idx, img in enumerate(images):
-            yield dict(img=img, label=int(labels[idx]))
-            if idx >= max_idx-1:
+            if idx >= max_idx - 1:
                 break
 
     return max_idx, iterate_images()
 
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 
-def open_mnist(images_gz: str, *, max_images: Optional[int]):
-    labels_gz = images_gz.replace('-images-idx3-ubyte.gz', '-labels-idx1-ubyte.gz')
-    assert labels_gz != images_gz
-    images = []
-    labels = []
+# (Similar comments as above for open_image_zip, open_lmdb, open_cifar10, open_mnist)
 
-    with gzip.open(images_gz, 'rb') as f:
-        images = np.frombuffer(f.read(), np.uint8, offset=16)
-    with gzip.open(labels_gz, 'rb') as f:
-        labels = np.frombuffer(f.read(), np.uint8, offset=8)
-
-    images = images.reshape(-1, 28, 28)
-    images = np.pad(images, [(0,0), (2,2), (2,2)], 'constant', constant_values=0)
-    assert images.shape == (60000, 32, 32) and images.dtype == np.uint8
-    assert labels.shape == (60000,) and labels.dtype == np.uint8
-    assert np.min(images) == 0 and np.max(images) == 255
-    assert np.min(labels) == 0 and np.max(labels) == 9
-
-    max_idx = maybe_min(len(images), max_images)
-
-    def iterate_images():
-        for idx, img in enumerate(images):
-            yield dict(img=img, label=int(labels[idx]))
-            if idx >= max_idx-1:
-                break
-
-    return max_idx, iterate_images()
-
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 
 def make_transform(
     transform: Optional[str],
     output_width: Optional[int],
     output_height: Optional[int]
 ) -> Callable[[np.ndarray], Optional[np.ndarray]]:
+    """Create a transform function based on the specified options."""
     def scale(width, height, img):
         w = img.shape[1]
         h = img.shape[0]
@@ -232,7 +119,7 @@ def make_transform(
 
     def center_crop(width, height, img):
         crop = np.min(img.shape[:2])
-        img = img[(img.shape[0] - crop) // 2 : (img.shape[0] + crop) // 2, (img.shape[1] - crop) // 2 : (img.shape[1] + crop) // 2]
+        img = img[(img.shape[0] - crop) // 2: (img.shape[0] + crop) // 2, (img.shape[1] - crop) // 2: (img.shape[1] + crop) // 2]
         img = PIL.Image.fromarray(img, 'RGB')
         img = img.resize((width, height), PIL.Image.LANCZOS)
         return np.array(img)
@@ -242,30 +129,31 @@ def make_transform(
         if img.shape[1] < width or ch < height:
             return None
 
-        img = img[(img.shape[0] - ch) // 2 : (img.shape[0] + ch) // 2]
+        img = img[(img.shape[0] - ch) // 2: (img.shape[0] + ch) // 2]
         img = PIL.Image.fromarray(img, 'RGB')
         img = img.resize((width, height), PIL.Image.LANCZOS)
         img = np.array(img)
 
         canvas = np.zeros([width, width, 3], dtype=np.uint8)
-        canvas[(width - height) // 2 : (width + height) // 2, :] = img
+        canvas[(width - height) // 2: (width + height) // 2, :] = img
         return canvas
 
     if transform is None:
         return functools.partial(scale, output_width, output_height)
     if transform == 'center-crop':
         if (output_width is None) or (output_height is None):
-            error ('must specify --resolution=WxH when using ' + transform + 'transform')
+            error('must specify --resolution=WxH when using ' + transform + 'transform')
         return functools.partial(center_crop, output_width, output_height)
     if transform == 'center-crop-wide':
         if (output_width is None) or (output_height is None):
-            error ('must specify --resolution=WxH when using ' + transform + ' transform')
+            error('must specify --resolution=WxH when using ' + transform + ' transform')
         return functools.partial(center_crop_wide, output_width, output_height)
     assert False, 'unknown transform'
 
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 
 def open_dataset(source, *, max_images: Optional[int]):
+    """Open a dataset from a source and return the number of files and an iterator."""
     if os.path.isdir(source):
         if source.rstrip('/').endswith('_lmdb'):
             return open_lmdb(source, max_images=max_images)
@@ -283,9 +171,10 @@ def open_dataset(source, *, max_images: Optional[int]):
     else:
         error(f'Missing input file or directory: {source}')
 
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 
 def open_dest(dest: str) -> Tuple[str, Callable[[str, Union[bytes, str]], None], Callable[[], None]]:
+    """Open the destination for writing based on the specified destination path."""
     dest_ext = file_ext(dest)
 
     if dest_ext == 'zip':
@@ -315,7 +204,7 @@ def open_dest(dest: str) -> Tuple[str, Callable[[str, Union[bytes, str]], None],
                 fout.write(data)
         return dest, folder_write_bytes, lambda: None
 
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 
 @click.command()
 @click.pass_context
@@ -332,65 +221,7 @@ def convert_dataset(
     transform: Optional[str],
     resolution: Optional[Tuple[int, int]]
 ):
-    """Convert an image dataset into a dataset archive usable with StyleGAN2 ADA PyTorch.
-
-    The input dataset format is guessed from the --source argument:
-
-    \b
-    --source *_lmdb/                    Load LSUN dataset
-    --source cifar-10-python.tar.gz     Load CIFAR-10 dataset
-    --source train-images-idx3-ubyte.gz Load MNIST dataset
-    --source path/                      Recursively load all images from path/
-    --source dataset.zip                Recursively load all images from dataset.zip
-
-    Specifying the output format and path:
-
-    \b
-    --dest /path/to/dir                 Save output files under /path/to/dir
-    --dest /path/to/dataset.zip         Save output files into /path/to/dataset.zip
-
-    The output dataset format can be either an image folder or an uncompressed zip archive.
-    Zip archives makes it easier to move datasets around file servers and clusters, and may
-    offer better training performance on network file systems.
-
-    Images within the dataset archive will be stored as uncompressed PNG.
-    Uncompressed PNGs can be efficiently decoded in the training loop.
-
-    Class labels are stored in a file called 'dataset.json' that is stored at the
-    dataset root folder.  This file has the following structure:
-
-    \b
-    {
-        "labels": [
-            ["00000/img00000000.png",6],
-            ["00000/img00000001.png",9],
-            ... repeated for every image in the dataset
-            ["00049/img00049999.png",1]
-        ]
-    }
-
-    If the 'dataset.json' file cannot be found, the dataset is interpreted as
-    not containing class labels.
-
-    Image scale/crop and resolution requirements:
-
-    Output images must be square-shaped and they must all have the same power-of-two
-    dimensions.
-
-    To scale arbitrary input image size to a specific width and height, use the
-    --resolution option.  Output resolution will be either the original
-    input resolution (if resolution was not specified) or the one specified with
-    --resolution option.
-
-    Use the --transform=center-crop or --transform=center-crop-wide options to apply a
-    center crop transform on the input image.  These options should be used with the
-    --resolution option.  For example:
-
-    \b
-    python dataset_tool.py --source LSUN/raw/cat_lmdb --dest /tmp/lsun_cat \\
-        --transform=center-crop-wide --resolution=512x384
-    """
-
+    """Convert an image dataset into a dataset archive usable with StyleGAN2 ADA PyTorch."""
     PIL.Image.init() # type: ignore
 
     if dest == '':
@@ -398,9 +229,6 @@ def convert_dataset(
 
     num_files, input_iter = open_dataset(source, max_images=max_images)
     archive_root_dir, save_bytes, close_dest = open_dest(dest)
-
-    if resolution is None: resolution = (None, None)
-    transform_image = make_transform(transform, *resolution)
 
     dataset_attrs = None
 
@@ -452,7 +280,8 @@ def convert_dataset(
     save_bytes(os.path.join(archive_root_dir, 'dataset.json'), json.dumps(metadata))
     close_dest()
 
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 
 if __name__ == "__main__":
     convert_dataset() # pylint: disable=no-value-for-parameter
+
